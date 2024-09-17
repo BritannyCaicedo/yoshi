8386db1ab4b062209eefd0afbb2cc6633700ddb3
Britanny_Caicedo <minaana897@gmail.com>
2024-09-17 18:59:00 -0500

    Create archivo.py

---
+++ archivo.py
@@ -0,0 +1,23 @@
+print ("Tabla de multiplicar del 1 hasta el 5 :)")
+print ("1 x 1 es:") 
+tab1 = 1
+resul1 = tab1
+print (resul1)
+print ("1 x 2 es:")
+tab2 = 2
+resul2 = tab2
+print (resul2)
+print ("1 x 3 es:")
+tab3 = 3
+resul3 = tab3
+print (resul3)
+print ("1 x 4 es:")
+tab4 = 4
+resul4 = tab4
+print (tab4)
+print ("1 x 5 es:")
+tab5 = 5
+resul5 = tab5
+print (resul5)
+print ("Gracias por haber visto, espero hayas aprendido =)")
+
